ffc91b3709d0b56d91a783739daad0c6cdccefc1
SenorYuen <adam.yuen@ucalgary.ca>
2024-08-01 17:27:43 -0600

    Altered the file organization

---
+++ MessageBoard/src/edu/ucalgary/ensf380/ScreenDisplay.java
@@ -0,0 +1,11 @@
+package edu.ucalgary.ensf380;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+public class ScreenDisplay {
+	
+}
