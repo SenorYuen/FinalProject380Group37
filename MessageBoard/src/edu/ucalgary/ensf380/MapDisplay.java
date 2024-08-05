ac6be31446be7956daa6ba09f3c09574c60ba18c
OmarAhmd10 <omar.ahmed3@ucalgary.ca>
2024-08-05 13:51:52 -0600

    added the jar files

--- MessageBoard/src/edu/ucalgary/ensf380/MapDisplay.java
+++ MessageBoard/src/edu/ucalgary/ensf380/MapDisplay.java
@@ -13,18 +13,19 @@ public class MapDisplay extends JPanel {
         // Initialize the MapDisplay with a list of trains and the current train
         this.trains = trains;
         this.currentTrain = currentTrain;
-        this.icon = new ImageIcon("data/Trains.png"); // Ensure the path is correct
+        this.icon = new ImageIcon("data/Trains.png"); // This gives the file directory to the image on the screen
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Set background color
+        //This sets the background of the screen to white
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        // Draw the subway map image in the center of the panel
+       
+        //This makes sure the image is in the center of the screen
         int imageX = 0, imageY = 0, imageWidth = 0, imageHeight = 0;
         if (icon.getImage() != null) {
             imageWidth = icon.getIconWidth();
@@ -40,53 +41,21 @@ public class MapDisplay extends JPanel {
         // Draw the trains with adjusted coordinates
         for (Train train : trains) {
             if (train == currentTrain) {
-                g.setColor(Color.GREEN); // Highlight the current train in green
+                g.setColor(Color.GREEN); //Whatever current train you choose as will become green
             } else {
-                g.setColor(Color.RED); // Other trains in red
+                g.setColor(Color.RED); //All the other trains will be red
             }
-            // Adjust train coordinates to fit the image position and scaling
-            int trainX = imageX + (int) (train.getX() * imageWidth / 1200.0); // Adjust based on original image scale
-            int trainY = imageY + (int) (train.getY() * imageHeight / 800.0); // Adjust based on original image scale
-            g.fillOval(trainX, trainY, 10, 10); // Draw the train as a filled oval
+            //This adjust the train coordinates so its in scale with its position
+            int trainX = imageX + (int) (train.getX() * imageWidth / 1200.0); 
+            int trainY = imageY + (int) (train.getY() * imageHeight / 800.0); 
+            g.fillOval(trainX, trainY, 10, 10); //This chooses the shape of the train on the screen
         }
     }
 
     public void updateTrains(List<Train> trains, Train currentTrain) {
-        // Update the list of trains and the current train, then repaint
+        //This will update the list of trains and the current train, then paint it again
         this.trains = trains;
         this.currentTrain = currentTrain;
         repaint();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
